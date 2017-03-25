500e01b87b6d598fceb2086cfb27b6471c20c239
Itamar Aviv <etamaraviv@gmail.com>
2017-03-25 11:25:26 +0000

    Added header, footer and bootstrap

--- wp-content/themes/customtheme/index.php
+++ wp-content/themes/customtheme/index.php
@@ -1,7 +1,21 @@
-<?php if ( have_posts() ) : ?>
-    <?php while ( have_posts() ) : the_post(); ?>
-        <h2>A Single Blog Post</h2>
-        <?php the_title(); ?>
-        <?php the_content(); ?>
-    <?php endwhile ?>
-<?php endif; ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+        <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+        <?php wp_head(); ?>
+    </head>
+    <body>
+        <?php if ( have_posts() ) : ?>
+            <?php while ( have_posts() ) : the_post(); ?>
+                <h2>A Single Blog Post</h2>
+                <?php the_title(); ?>
+                <?php the_content(); ?>
+            <?php endwhile ?>
+        <?php endif; ?>
+        <?php wp_footer(); ?>
+    </body>
+</html>
