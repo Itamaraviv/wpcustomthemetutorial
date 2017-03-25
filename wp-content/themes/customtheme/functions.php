1f8d17585b17d7b135d74ee91208b63402251d33
Itamar Aviv <etamaraviv@gmail.com>
2017-03-25 17:33:22 +0000

    Added title and dynamic navbar menu

--- wp-content/themes/customtheme/functions.php
+++ wp-content/themes/customtheme/functions.php
@@ -1,5 +1,5 @@
 <?php
-//Loading in stylesheets
+//Loading in scripts
 function enqueue_customtheme_styles() {
     wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css");
     wp_enqueue_style("customtheme-style", get_stylesheet_uri());
@@ -8,9 +8,13 @@ add_action("wp_enqueue_scripts", "enqueue_customtheme_styles");
 
 
 function customtheme_setup() {
+    
+    //Register a custom primary navigation menu
     register_nav_menus( array(
         "primary" => __("Primary Menu", "customtheme")
         ));
+        //Add theme support for document title tag
+        add_theme_support( "title-tag" );
 }
 add_action("after_setup_theme", "customtheme_setup");
 ?>
