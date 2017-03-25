4cd9c3363f19e8942b7d419e10419ceb017ac509
Itamar Aviv <etamaraviv@gmail.com>
2017-03-25 18:43:12 +0000

    Added Bootstrap js and seperated header from  index into header partial

--- wp-content/themes/customtheme/functions.php
+++ wp-content/themes/customtheme/functions.php
@@ -1,20 +1,19 @@
 <?php
 //Loading in scripts
 function enqueue_customtheme_styles() {
-    wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css");
-    wp_enqueue_style("customtheme-style", get_stylesheet_uri());
+    wp_enqueue_script('bootstrap-js', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js', array('jquery'));
+    wp_enqueue_style('bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css');
+    wp_enqueue_style('customtheme-style', get_stylesheet_uri() );
 }
-add_action("wp_enqueue_scripts", "enqueue_customtheme_styles");
-
+add_action('wp_enqueue_scripts', 'enqueue_customtheme_styles');
 
 function customtheme_setup() {
-    
     //Register a custom primary navigation menu
     register_nav_menus( array(
-        "primary" => __("Primary Menu", "customtheme")
-        ));
-        //Add theme support for document title tag
-        add_theme_support( "title-tag" );
+        'primary' => __( 'Primary Menu', 'customtheme' )
+    ));
+    //Add theme support for document title tag
+    add_theme_support( 'title-tag' );
 }
-add_action("after_setup_theme", "customtheme_setup");
+add_action( 'after_setup_theme', 'customtheme_setup' );
 ?>
