0272a82c4fbe226a521cdaff4738b115ac477efc
Itamar Aviv <etamaraviv@gmail.com>
2017-03-25 14:41:11 +0000

    Added code comments

--- wp-content/themes/customtheme/functions.php
+++ wp-content/themes/customtheme/functions.php
@@ -1,4 +1,5 @@
 <?php
+//Loading in stylesheets
 function enqueue_customtheme_styles() {
     wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css");
     wp_enqueue_style("customtheme-style", get_stylesheet_uri());
--- wp-content/themes/customtheme/index.php
+++ wp-content/themes/customtheme/index.php
@@ -2,7 +2,6 @@
 <html <?php language_attributes(); ?>>
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>" />
-        <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">-->
         <link rel="profile" href="http://gmpg.org/xfn/11" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
         <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
