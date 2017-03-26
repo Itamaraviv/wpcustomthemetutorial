84c58472a9a33917a583417b94dcfec35f772b08
Itamar Aviv <etamaraviv@gmail.com>
2017-03-26 13:13:16 +0000

    Added blog posts navigation, date display and changed posts layout

--- wp-content/themes/customtheme/single.php
+++ wp-content/themes/customtheme/single.php
@@ -10,14 +10,27 @@
  
  get_header(); ?>
 <div class="container">
-    <?php if ( have_posts() ) : ?>
-        <?php while ( have_posts() ) : the_post(); ?>
-            <h2>A Single Blog Post</h2>
-            <?php the_title(); ?>
-            <?php the_content(); ?>
-        <?php endwhile ?>
-    <?php endif; ?>
-    
-    <?php get_sidebar(); ?>
-</div>
+    <div class="row">
+        <div class="col-md-8">
+            <?php if ( have_posts() ) : ?>
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <h1><?php the_title(); ?></h1>
+                    <p><?php the_date(); ?></p>
+                    <?php the_content(); ?>
+                <?php endwhile ?>
+            <?php endif; ?>
+            
+            <?php if (is_singular ('post') ) {
+                the_post_navigation( array(
+                    'next_text' => 'Next',
+                    'prev_text' => 'Previous'
+                    ) );
+                }
+            ?>
+        </div>
+        <div class="col-md-4">
+            <?php get_sidebar(); ?>
+        </div>
+    </div> <!-- .row -->
+</div> <!-- .container -->
 <?php get_footer(); ?>
